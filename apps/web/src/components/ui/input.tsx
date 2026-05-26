6defd20715fdbcbf592681196144b1dcb91bbb7d
VinothPrinzz <VinothPrinzz@gmail.com>
2026-05-26 14:40:57 +0530

    products update - notmarking zero fix

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, onWheel, ...props }, ref) => {
     return (
       <input
         type={type}
@@ -13,6 +13,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className
         )}
         ref={ref}
+        onWheel={
+          type === "number"
+            ? e => { e.currentTarget.blur(); onWheel?.(e); }
+            : onWheel
+        }
         {...props}
       />
     );
